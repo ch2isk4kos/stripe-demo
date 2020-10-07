a817074d034ca35f8689a36719c4d32f7595a223
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-07 13:44:20 -0400

    added comments

--- stripe-server/index.js
+++ stripe-server/index.js
@@ -1,14 +1,14 @@
 const cors = require("cors");
 const express = require("express");
-const stripe = require("stripe")(""); // @TODO: add a stripe key
-const uuid = require("uuid/v4");
+//const stripe = require("stripe")(""); // @TODO: add a stripe key
+const uuid = require("uuid");
 const PORT = 3000;
 
 const app = express();
 
 // MIDDLEWARE
-app.use(express.json());              // passx along json values
-app.use(cors());                      // cross origin resource sharing
+app.use(express.json());                // passx along json values
+app.use(cors());                        // cross origin resource sharing
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -16,4 +16,4 @@ app.get("/", (req, res) => {
 })
 
 // SERVER
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
