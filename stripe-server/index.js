22a56eeb28825f9ac8a69f144ee4509eacd99cf5
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-07 14:38:06 -0400

    added a post action method and destructured request variables

--- stripe-server/index.js
+++ stripe-server/index.js
@@ -1,5 +1,6 @@
 const cors = require("cors");
 const express = require("express");
+const { default: Stripe } = require("stripe");
 //const stripe = require("stripe")(""); // @TODO: add a stripe key
 const uuid = require("uuid");
 const PORT = 3000;
@@ -7,12 +8,16 @@ const PORT = 3000;
 const app = express();
 
 // MIDDLEWARE
-app.use(express.json());                // passx along json values
-app.use(cors());                        // cross origin resource sharing
+app.use(express.json());                        // passx along json values
+app.use(cors());                                // cross origin resource sharing
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Microphone Check.")
+})
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;          // destructure a product and a token from client
 })
 
 // SERVER
