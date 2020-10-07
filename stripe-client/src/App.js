04de71c3449b1d24f4269f1462fac814024eb7c4
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-07 16:10:20 -0400

    imported stripe checkout module and added child component

--- stripe-client/src/App.js
+++ stripe-client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import StripeCheckout from "react-stripe-checkout";
 
 function App() {
-
   const [product, setProduct] = useState({
     name: "React T-shirt",
     price: 35,
@@ -16,9 +15,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <StripeCheckout stripeKey={""} token={""} name={product.name}>
-          
-        </StripeCheckout>
+        <StripeCheckout 
+          stripeKey={process.env.REACT_APP_KEY} 
+          token={handlePayment} 
+          name={product.name}
+          amount={product.price * 100} />          
       </header>
     </div>
   );
