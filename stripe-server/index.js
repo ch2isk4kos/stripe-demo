354ba3412fd950cdbd7e838b855318ba56ece5f8
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-07 14:40:38 -0400

    pointed a variable to a randomly generated number

--- stripe-server/index.js
+++ stripe-server/index.js
@@ -18,6 +18,7 @@ app.get("/", (req, res) => {
 
 app.post("/payment", (req, res) => {
   const { product, token } = req.body;          // destructure a product and a token from client
+  const idempontencyKey = uuid();               // assigns a unique key that will prevent user from being charged more than once.
 })
 
 // SERVER
